ab23b87e905db3e934984dc52ceaffd18ee7af6a
Jūlija Tatarinceva <huducn@gmail.com>
2024-10-23 15:16:07 +0300

    Cleaned up a little and switched from mesh creation to a polygon collider creation

--- Assets/Scripts/General/DimensionSwitcher.cs
+++ Assets/Scripts/General/DimensionSwitcher.cs
@@ -4,10 +4,8 @@ using System.Linq;
 
 public class DimensionSwitcher : MonoBehaviour {
     public Transform player;
-    private Plane _slicingPlane;
-    public GameObject obj1;
-    public GameObject obj2;
-    public GameObject obj3;
+    private Plane _slicingPlane; // The plane that is used for slicing 3D objects when switching from 3D to 2D
+    public GameObject[] slicableObjects = new GameObject[1]; //All the objects that will be sliced after dimension switch, can be modified in th editor
     // Store the intersection points
     private List<Vector3> _intersectionPoints = new List<Vector3>();
 
@@ -19,12 +17,10 @@ public class DimensionSwitcher : MonoBehaviour {
             Debug.DrawLine(player.position, player.position + forwardDirection, Color.green);
             DrawSlicingPlane(_slicingPlane, player.position);
             // Slice the object and generate 2D geometry
-            SliceObject(obj1);
-            Generate2DPolygonFromIntersections(_intersectionPoints);
-            SliceObject(obj2);
-            Generate2DPolygonFromIntersections(_intersectionPoints);
-            SliceObject(obj3);
-            Generate2DPolygonFromIntersections(_intersectionPoints);
+            foreach (GameObject objectToSlice in slicableObjects){
+                SliceObject(objectToSlice);
+                Generate2DPolygonFromIntersections(_intersectionPoints);
+            }
         }
     }
     
@@ -65,13 +61,20 @@ public class DimensionSwitcher : MonoBehaviour {
 
         // Convert local vertices to world space, including scaling
         Vector3[] worldVertices = new Vector3[localVertices.Length];
-        for (int i = 0; i < localVertices.Length; i++)
-            // Transform the local vertex by the object's local-to-world matrix
+        for (int i = 0; i < localVertices.Length; i++) // Transform the local vertex by the object's local-to-world matrix
             worldVertices[i] = localToWorld.MultiplyPoint3x4(localVertices[i]);
         int[] triangles = mesh.triangles;
 
         // Clear previous intersection points
         _intersectionPoints.Clear();
+        
+        // Defining the slicing plane's local coordinate system (planeRight and planeUp)
+        // If needed to stop projecting slices, this block can be commented out
+        Vector3 planeNormal = _slicingPlane.normal;
+        Vector3 planeRight = Vector3.Cross(planeNormal, Vector3.up).normalized;
+        if (planeRight == Vector3.zero)
+            planeRight = Vector3.Cross(planeNormal, Vector3.forward).normalized;
+        Vector3 planeUp = Vector3.Cross(planeNormal, planeRight).normalized;
 
         // Iterate over all triangles in the mesh
         for (int i = 0; i < triangles.Length; i += 3){
@@ -84,19 +87,19 @@ public class DimensionSwitcher : MonoBehaviour {
             bool v1Above = _slicingPlane.GetSide(v1);
             bool v2Above = _slicingPlane.GetSide(v2);
             
-            // Check if this triangle intersects the slicing plane
-            // Find the intersection points on the triangle's edges
+            // Checking if this triangle intersects the slicing plane & finding the intersection points on the triangle's edges
+            // If needed to stop projecting slices, (ProjectTo2D(intersection, planeRight, planeUp)) can be switched to (intersection)
             if (v0Above != v1Above){
                 Vector3 intersection = FindIntersection(v0, v1);
-                _intersectionPoints.Add(intersection);
+                _intersectionPoints.Add(ProjectTo2D(intersection, planeRight, planeUp));
             }
             if (v1Above != v2Above){
                 Vector3 intersection = FindIntersection(v1, v2);
-                _intersectionPoints.Add(intersection);
+                _intersectionPoints.Add(ProjectTo2D(intersection, planeRight, planeUp));
             }
             if (v2Above != v0Above){
                 Vector3 intersection = FindIntersection(v2, v0);
-                _intersectionPoints.Add(intersection);
+                _intersectionPoints.Add(ProjectTo2D(intersection, planeRight, planeUp));
             }
         }
     }
@@ -111,37 +114,25 @@ public class DimensionSwitcher : MonoBehaviour {
     
     // It is needed to connect the intersection points in a 2D space to create the polygon, triangulate the polygon for rendering in Unity
     void Generate2DPolygonFromIntersections(List<Vector3> polygon2D){
-        if (_intersectionPoints.Count < 3) return; // There is nothing to generate since we need 3 points for at least one triangle
-        // Create a new GameObject for the 2D mesh
-        GameObject polygonObject = new GameObject("Sliced2DPolygon", typeof(MeshFilter), typeof(MeshRenderer), typeof(PolygonCollider2D));
-        // Placeholder material for the polygon
-        polygonObject.GetComponent<MeshRenderer>().material = new Material(Shader.Find("Sprites/Default"));
+        if (polygon2D.Count < 3) return; // Need at least 3 points to create a polygon
 
+        // Create a new GameObject for the 2D polygon
+        GameObject polygonObject = new GameObject("Sliced2DPolygon", typeof(PolygonCollider2D));
+
+        // Sort vertices to ensure correct triangulation
         CleanupVertices(ref polygon2D);
         SortVerticesClockwise(ref polygon2D);
-        List<int> triangles = new List<int>();
-        for (int i = 1; i < polygon2D.Count - 1; i++){
-            triangles.Add(0);
-            triangles.Add(i);
-            triangles.Add(i + 1);
-        }
         
-        // Creating the 2D mesh
-        Mesh polygonMesh = new Mesh {
-            vertices = polygon2D.ToArray(),
-            triangles = triangles.ToArray()
-        };
-        polygonMesh.RecalculateNormals();
-        polygonMesh.RecalculateBounds();
-
-        // Assign the created mesh to the MeshFilter
-        polygonObject.GetComponent<MeshFilter>().mesh = polygonMesh;
+        // Create a 2D collider using the 2D vertices
+        Vector2[] colliderPoints = new Vector2[polygon2D.Count];
+        for (int i = 0; i < polygon2D.Count; i++)
+            colliderPoints[i] = polygon2D[i];
+        polygonObject.GetComponent<PolygonCollider2D>().points = colliderPoints;
     }
 
+
     void CleanupVertices(ref List<Vector3> polygon2D) {
-        // Triangulation (assumes the polygon is convex and ordered correctly)
-        for (int i = 0; i < polygon2D.Count; i++) {
-            if (i < 2) continue; // We can't do any math until we are deeper in the list
+        for (int i = 2; i < polygon2D.Count; i++) {
             // Calculate the area of the triangle formed by the points, if it is zero, the points are collinear
             float area = polygon2D[i-2].x * (polygon2D[i-1].y - polygon2D[i].y) +
                          polygon2D[i-1].x * (polygon2D[i].y - polygon2D[i-2].y) +
@@ -153,22 +144,22 @@ public class DimensionSwitcher : MonoBehaviour {
             float d3 = Vector3.Distance(polygon2D[i-2], polygon2D[i]);
 
             // Check which point is in the middle
-            if (Mathf.Approximately(d1 + d2, d3))
-                // polygon2D[i-1] is in the middle
-                for (int j = i-1; j < polygon2D.Count - 1; j++) polygon2D[j] = polygon2D[j + 1];
-            else if (Mathf.Approximately(d1 + d3, d2))
-                // polygon2D[i-2] is in the middle
-                for (int j = i-2; j < polygon2D.Count - 1; j++) polygon2D[j] = polygon2D[j + 1];
-            else
-                // polygon2D[i] is in the middle
-                for (int j = i; j < polygon2D.Count - 1; j++) polygon2D[j] = polygon2D[j + 1];
+            if (Mathf.Approximately(d1 + d2, d3))      // polygon2D[i-1] is in the middle
+                for (int j = i-1; j < polygon2D.Count - 1; j++) 
+                    polygon2D[j] = polygon2D[j + 1];
+            else if (Mathf.Approximately(d1 + d3, d2)) // polygon2D[i-2] is in the middle
+                for (int j = i-2; j < polygon2D.Count - 1; j++) 
+                    polygon2D[j] = polygon2D[j + 1];
+            else                                       // polygon2D[i] is in the middle
+                for (int j = i; j < polygon2D.Count - 1; j++) 
+                    polygon2D[j] = polygon2D[j + 1];
             
             // Remove the last element since it's now a duplicate after shifting
             polygon2D.RemoveAt(polygon2D.Count - 1);
             // Decrement 'i' to recheck the current position (since it now contains the next element)
             i--;
         }
-        // Getting rid of duplicates and sorting the vertices to create triangles more effectively
+        // Getting rid of duplicates 
         polygon2D = polygon2D.Distinct().ToList();
     }
     
@@ -185,5 +176,13 @@ public class DimensionSwitcher : MonoBehaviour {
             float angleB = Mathf.Atan2(b.y - centroid.y, b.x - centroid.x);
             return angleA.CompareTo(angleB);
         });
+    }
+    // Function to project a 3D point onto the slicing plane's 2D space
+    Vector2 ProjectTo2D(Vector3 point, Vector3 planeRight, Vector3 planeUp) {
+        // Convert the point into the plane's local 2D coordinate system
+        Vector3 localPoint = point - player.position; // Translate relative to plane origin
+        float x = Vector3.Dot(localPoint, planeRight); // X-coordinate
+        float y = Vector3.Dot(localPoint, planeUp);    // Y-coordinate
+        return new Vector2(x, y);                      // Return as 2D point
     }
 }
